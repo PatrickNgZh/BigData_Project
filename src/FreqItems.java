6753323d45609ad977486d9618f15fb326cebc53
steffenver <steffen@vering.de>
2017-01-13 16:09:56 +0000

    First item count implementation using MapReduce

--- src/FreqItems.java
+++ src/FreqItems.java
@@ -1,9 +1,36 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;  
+import org.apache.hadoop.mapreduce.Job;  
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;  
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;  
 
 public class FreqItems {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+	public static void main(String[] args) throws Exception {
+		FileSystem fs = FileSystem.get(new Configuration());
+		fs.delete(new Path("C:/Users/steff/git/MapReduce_apriori_SV_VR/out"), true);
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		if (otherArgs.length != 2) {
+			System.err.println("Usage: Freq items <in> <out>");
+			System.exit(2);
+		}
+		@SuppressWarnings("deprecation")
+		Job job = new Job(conf, "freq items");
+		job.setJarByClass(FreqItems.class);
+		job.setMapperClass(basketMapper.class);
+		//job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(itemReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 
 }
+
---
+++ src/basketMapper.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class basketMapper extends Mapper<Object, Text, Text, IntWritable> {
+	
+	private Text item = new Text();
+	private final static IntWritable one = new IntWritable(1);
+
+	public void map(Object key, Text value , Context context) throws IOException, InterruptedException {
+		String text = value.toString();
+		String [] input = text.split(",");
+		
+		for (int i=0;i<input.length;i++){
+			item.set(input[i]);
+			context.write(item, one);			
+		}
+
+	}
+
+}
